e39597b70643e9531e327a2da0bfc5183db9250c
$Aditya@ <54371583+Xzooan@users.noreply.github.com>
2019-09-28 20:33:47 +0530

    Profit and loss finder
    Only rupees

---
+++ Profit and loss finder.py
@@ -0,0 +1,17 @@
+#created by Aditya Srivastava
+sp=float(input("The selling price="))
+print(sp)
+cp=float(input("The cost price="))
+print(cp)
+if sp>cp:
+   profit=sp-cp
+   print("Profit",'=',profit,'rupees')
+   _profit=profit*100/cp
+   print("Profit %",'=',_profit)
+else:
+   loss=cp-sp
+   print("Loss",'=',loss,'rupees')
+   _loss=loss*100/cp
+   print("Loss %",'=',_loss)
+ 
+print("=========end=========") 
